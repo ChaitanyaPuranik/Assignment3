b88477ecc472ba3512ccd6c1e7b8fcc60787577a
FahadZaffy <zafarfa@sheridancollege.ca>
2025-11-30 20:03:57 -0500

    finished Product Controller method setup for views

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -15,11 +15,98 @@ namespace ProductRegistration_Group.Controllers
         {
             List<Product> products = new List<Product>();
             HttpResponseMessage response = client.GetAsync(url).Result;
-            if(response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
                 products = response.Content.ReadFromJsonAsync<List<Product>>().Result;
             }
             return View(products);
         }
+
+        //details 
+        public IActionResult Details(int id)
+        {
+            Product product = null;
+            HttpResponseMessage response = client.GetAsync($"{url}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                product = response.Content.ReadFromJsonAsync<Product>().Result;
+            }
+
+            return View(product);
+        }
+
+
+        //Create [get]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Create [post]
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            HttpResponseMessage response = client.PostAsJsonAsync(url, product).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+
+        //Edit [get]
+
+        public IActionResult Edit(int id)
+        {
+            Product product = null;
+            HttpResponseMessage response = client.GetAsync($"{url}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                product = response.Content.ReadFromJsonAsync<Product>().Result;
+            }
+
+
+            return View(product);
+        }
+
+        //Edit [Post]
+
+        [HttpPost]
+        public IActionResult Edit(int id, Product product)
+        {
+            HttpResponseMessage response = client.PutAsJsonAsync($"{url}/{id}", product).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //Delete [get] 
+        public IActionResult Delete(int id)
+        {
+            Product product = null;
+            HttpResponseMessage response = client.GetAsync($"{url}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                product = response.Content.ReadFromJsonAsync<Product>().Result;
+            }
+            return View(product);
+        }
+
+
+        //Delete [post]
+        [HttpPost]
+        public IActionResult Delete(int id, Product product)
+        {
+            HttpResponseMessage response = client.DeleteAsync($"{url}/{id}").Result;
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
